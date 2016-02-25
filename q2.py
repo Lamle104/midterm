e2acce949e7c2cb95e9a33de11092ac19efb2a8a
Lam Le <LamMinhLe@Lams-MacBook-Pro.local>
2016-02-24 22:01:04 -0500

    midterm

--- q2.py
+++ q2.py
@@ -28,7 +28,12 @@ def askUserChoice():
     # 1. Asks the user to enter their input (between 1 and 4)
     # 2. Checks that the user's input is valid. If it's not valid (if it's not between 1 and 4), then ask the user to re-enter their input.
     # 3. Once the user enters a valid input, return that input as an integer.
- 
+    print('How many sticks would you like to pick up? Choose between 1 and 4') #Asks user how many sticks they would like to pick up
+    userChoice = input() #Users choice 
+    if userChoice in range(1,4): #If the users choice is between 1 and 4. Continue.
+        return userChoice #Returns the users choice
+    else: #Else statement saying that if you choose a number outside of 1 and 4. It is invalid.
+        print('sorry your choice is invalid. Please re-enter your input.') #Telling the user that your input is invalid.
     
 def subtractSticks( number ):
     
@@ -37,8 +42,14 @@ def subtractSticks( number ):
     # 1. subtracts the parameter `number` from the global variable `sticks`
     # 2. checks if the number subtracted resulted in the last stick, if so, return True
     # 3. if there are still sticks left, return False
+    sticks = sticks - number #subtracts the number from the variable sticks
+    if sticks <=0: #if statement stating if the number subtracted resulted in the last trick, its True 
+        return True 
+    else: #else statement stating if there are sticks left, its False
+        return False
  
     
 def determineComputerChoice():
     # TODO: write code inside this function that returns an integer between 1 and 4, random chosen by the computer
- 
+    computerChoice = random.randint(1,4) #returns a random integer between 1 and 4 chosen by the computer
+    return computerChoice
