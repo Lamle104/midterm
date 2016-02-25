fdaf6698e4f7ddb38ec16586d5ca71333a7595fb
Lam Le <LamMinhLe@Lams-MacBook-Pro.local>
2016-02-24 21:10:57 -0500

    q1

--- q1.py
+++ q1.py
@@ -1,8 +1,21 @@
-# A credit card company computes a customer's "minimum payment" according to the following rule. The minimum payment is equal to either $10 or 2.1% of the customer's balance, whichever is greater; however, if a $10 minimum payment exceeds the balance, then the minimum payment is the balance. Write a program to return the minimum payment. Assume that the variable balance contains the customer's balance.
+#Lam Le
+
+#   A credit card company computes a customer's "minimum payment" according to the following rule.
+#   The minimum payment is equal to either $10 or 2.1% of the customer's balance, whichever is greater;
+#   however, if a $10 minimum payment exceeds the balance, then the minimum payment is the balance. Write a program to return the minimum payment.
+#   Assume that the variable balance contains the customer's balance.
 #   Example 1: if your balance is 1000, then your program should return 21. 
 #   Example 2: if your balance is 600, then your program should return 12.6. 
 #   Example 3: if your balance is 25, then your program should return 10. 
 #   Example 4: if your balance is 8, then your program should return 8. 
 
-def computeMinimumPayment( balance ):
+def computeMinimumPayment( balance ): 
     #TODO write code inside this function that achieves the functionality described above
+
+    if balance > 10:  #if balance is greater than 10 or 2.1% of customers balance
+        return balance * 0.021 #return balance times 0.021 is the customers minimum payment.
+    elif balance < 10: #else if 10 is greater than the balance. 
+        return balance #return minimum payment the same as balance
+    else:
+        return 10 #return 10 dollar minimum payment
+    
